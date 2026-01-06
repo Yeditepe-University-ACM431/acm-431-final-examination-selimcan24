b7fb136093ad21c0c809b2e0f415a37f891d8114
Selim Can Aydın <selimcan.aydin@std.yeditepe.edu.tr>
2026-01-06 15:49:13 +0300

    Refactor AppNavGraph to include composable functions

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -1,21 +1,39 @@
 package com.yeditepe.finalexam.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.yeditepe.finalexam.ui.TaskDetailScreen
+import com.yeditepe.finalexam.ui.TaskListScreen
 
 @Composable
 fun AppNavGraph() {
 
-    val navController = rememberNavController()
+    @Composable
+    fun AppNavGraph() {
+        val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = "tasks"
-    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "tasks"
+        ) {
+            composable("tasks") {
+                TaskListScreen(navController)
 
-        // TODO 1: Add composable for "tasks"
+            }
 
-        // TODO 2: Add composable for "taskDetail/{title}"
+            composable(
+                route = "taskDetail/{title}",
+                arguments = listOf(navArgument("title") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val title = backStackEntry.arguments?.getString("title") ?: ""
+                TaskDetailScreen(title)
+            }
+        }
     }
 }
+
+//
